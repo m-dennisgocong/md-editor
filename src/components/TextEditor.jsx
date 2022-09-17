ca3874b0428498b474fceec7fddd76021c05c9f4
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-09-17 18:17:16 +0700

    close #2 add TextEditor component with textarea
    handle textarea with a corresponding id="editor"

---
+++ src/components/TextEditor.jsx
@@ -0,0 +1,9 @@
+const TextEditor = () => {
+
+    return(
+        <>
+            <textarea name="editor" id="editor" cols="45" rows="35"></textarea>
+        </>
+    );
+}
+export default TextEditor;
