554007ed83bc7d42eeb1a728c173628400b8dfe1
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-09-17 18:20:37 +0700

    import components and add state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import { useState } from 'react'
-
+import TextEditor from './components/TextEditor';
+import PreviewText from './components/PreviewText';
 function App() {
+
+  const [inputText, setInputText] = useState("");
+  const [singleView, setSingleView] = useState(true);
 
   return (
     <div className="App">
-     
+      <header>
+        <ul>
+          <li>Editor</li>
+          <li>Preview</li>
+          <li>Toolbox</li>
+        </ul>
+      </header>
+      <main>
+        <TextEditor />
+        {!singleView && <PreviewText />}
+      </main>
     </div>
   )
 }
