7d2e71a8b0357a673925f545adb2478edd4b873c
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-09-18 10:42:55 +0700

    close #5 import marked and prismjs

--- src/components/PreviewText.jsx
+++ src/components/PreviewText.jsx
@@ -1,12 +1,34 @@
+import { marked } from 'marked';
+import Prism from 'prismjs';
 
+const renderer = new marked.Renderer();
+renderer.code = function(code, lang, escaped) {
+  code = this.options.highlight(code, lang);
+  if (!lang) {
+    return `<pre><code>${code}</code></pre>`;
+  }
+
+  let langClass = "language-" + lang;
+  return `<pre class="${langClass}"><code class="${langClass}">${code}</code></pre>`;
+};
+
+marked.setOptions({
+    renderer,
+    breaks: true,
+    highlight: function (code, lang) {
+        try{
+            return Prism.highlight(code, Prism.languages[lang], lang);
+        }
+        catch{
+            return code;
+        }
+    }
+});
 
 const PreviewText = ({inputText}) => {
-
     return(
-        <div id="preview">
-            {inputText}
-        </div>
-    )
+        <div dangerouslySetInnerHTML={{__html: marked(inputText)}} id="preview"></div>
+    );
 }
 
 export default PreviewText;
