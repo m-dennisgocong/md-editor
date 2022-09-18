0098c04fb13d47fc63a70739740c7157dddab2dc
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-09-18 17:52:09 +0700

    separate the initialText

---
+++ src/components/initialText.js
@@ -0,0 +1,30 @@
+export const initialText = 
+`# H1
+## H2
+### H3
+
+**bold text**
+
+*italicized text*
+
+> blockquote
+
+1. First item
+2. Second item
+3. Third item
+
+- First item
+- Second item
+- Third item
+
+\` inline code\`
+
+\`\`\`
+  function display(){
+    console.log("blocks code");
+  }
+\`\`\`
+
+[md-edit](https://github.com/m-dennisgocong/md-editor)
+
+![alt text](./../public/markdown_logo.png)`;
