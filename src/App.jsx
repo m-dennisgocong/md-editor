9db11ec5077abecea987af296fc2348d458f394c
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-09-21 01:24:01 +0700

    import initialText

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,10 @@ import { useState } from 'react'
 import TextEditor from './components/TextEditor';
 import PreviewText from './components/PreviewText';
 import HeaderMenu from './components/HeaderMenu';
+import {initialText} from './initialText.js'
 import './App.scss';
 function App() {
-const initialText = 
-`# H1
-## H2
-### H3
 
-**bold text**
-
-*italicized text*
-
-> blockquote
-
-1. First item
-2. Second item
-3. Third item
-
-- First item
-- Second item
-- Third item
-
-\` inline code\`
-
-\`\`\`
-  function display(){
-    console.log("blocks code");
-  }
-\`\`\`
-
-[md-edit](https://github.com/m-dennisgocong/md-editor)`;
   const [inputText, setInputText] = useState(initialText);
   const [typeView, setTypeView] = useState(true);
   const [screenType, setScreenType] = useState(false);
