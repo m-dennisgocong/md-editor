468c8bd93ebbbd5457625af38ba9d2c633c35765
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-09-21 01:23:27 +0700

    separate  initialText to a different file

---
+++ src/initialText.js
@@ -0,0 +1,30 @@
+export const initialText = 
+`# H1
+## H2
+### H3
+
+**bold text**
+
+*italicized text*
+
+> blockquote
+
+1. First item
+2. Second item
+3. Third item
+
+- First item
+- Second item
+- Third item
+
+\` inline code\`
+
+\`\`\`
+  function display(){
+    console.log("blocks code");
+  }
+\`\`\`
+
+![alt text](./public/markdown_logo.png)
+
+[md-edit](https://github.com/m-dennisgocong/md-editor)`;
